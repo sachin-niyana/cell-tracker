8598aa9c24b62a1f21eab6ac3bcea9fb3ea93942
sachin-niyana <sachinjakhar100@gmail.com>
2024-06-03 16:56:33 +0530

    add talwind classes

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="text-red-500 text-center text-6xl font-bold">
+      Hello World
+    </div>
+  );
+};
 
-export default Header
+export default Header;
