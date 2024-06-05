8d03450dcd1c67e96ec8890cdd8d03e7db5dc3cc
sachin-niyana <sachinjakhar100@gmail.com>
2024-06-05 09:09:41 +0530

    merge code

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../common/Header";
+import Advantage from "./Advantage";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Advantage />
+    </>
+  );
+};
+
+export default Home;
